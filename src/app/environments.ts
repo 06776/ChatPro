1f2a17b5b71111b3694db4d11951d22f10ca3c09
06776 <hajdarapatrik11@outlook.com>
2023-10-16 09:14:21 +0200

    Angular authentication

---
+++ src/app/environments.ts
@@ -0,0 +1,13 @@
+export const Environments = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBZVUp6dnhKI37Lrs7vQCxLzlRJTjuOrOg",
+        authDomain: "frontend-a2f5c.firebaseapp.com",
+        databaseURL: "https://frontend-a2f5c-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "frontend-a2f5c",
+        storageBucket: "frontend-a2f5c.appspot.com",
+        messagingSenderId: "958465238910",
+        appId: "1:958465238910:web:3a21283a9f9e8877e4f52f",
+        measurementId: "G-MTW0713RER"
+    }
+}
